a034580231a555480299e425160c534db2cbbb5e
Giulia Pelosi <giulia.pelosi@dgsspa.com>
2024-02-01 10:19:23 +0100

    init commit

---
+++ src/main/java/com/dgsspa/snake/SnakeVaadinExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dgsspa.snake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeVaadinExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeVaadinExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgsspa/snake/SnakeVaadinExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsspa.snake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeVaadinExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
