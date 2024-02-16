7a571e5d513f2cb4d06956d554ddcc96baeba926
Giulia Pelosi <giulia.pelosi@dgsspa.com>
2024-02-16 11:38:50 +0100

    completed game - final commit

---
+++ src/main/java/com/dgsspa/snake/enums/Direction.java
@@ -0,0 +1,8 @@
+package com.dgsspa.snake.enums;
+
+public enum Direction {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
+}
---
+++ src/main/java/com/dgsspa/snake/utils/Point.java
@@ -0,0 +1,13 @@
+package com.dgsspa.snake.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Point {
+    private int xc;
+    private int yc;
+
+
+}
